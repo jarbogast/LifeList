e0e868f748acac17e857ae6ca1700fb9489d2817
Jonathan Arbogast <jonathan.arbogast@softwarebyassurance.com>
2018-02-04 19:50:20 -0500

    Fixed SwiftLint issues. Added Travis.

--- Lifelist/AppDelegate.swift
+++ Lifelist/AppDelegate.swift
@@ -14,29 +14,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let container = NSPersistentContainer(name: "Lifel ist")
-        container.loadPersistentStores { (description, error) in
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let container = NSPersistentContainer(name: "Lifelist")
+        container.loadPersistentStores { (_, error) in
             if let error = error {
                 fatalError("Failed to load Core Data stack: \(error)")
             } else {
                 self.distributeModelToViewController(controller: self.window?.rootViewController, container: container)
             }
         }
-        
+
         return true
     }
 
     func distributeModelToViewController(controller: UIViewController?, container: NSPersistentContainer) {
         guard let controller = controller else { return }
-        
+
         if let modelController = controller as? LifelistController {
             modelController.persistentContainer = container
         }
-        
+
         for childController in controller.childViewControllers {
             distributeModelToViewController(controller: childController, container: container)
         }
     }
 }
-
--- Lifelist/SightingsViewController.swift
+++ Lifelist/SightingsViewController.swift
@@ -10,36 +10,42 @@ import UIKit
 import CoreData
 
 class SightingsViewController: UITableViewController, LifelistController {
-    
+
     var fetchedResultsController: NSFetchedResultsController<Sighting>?
-    
+
     var persistentContainer: NSPersistentContainer? {
         didSet {
             let fetchRequest = NSFetchRequest<Sighting>(entityName: "Sighting")
             if let context = persistentContainer?.viewContext {
-                fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+                fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                                                      managedObjectContext: context,
+                                                                      sectionNameKeyPath: nil, cacheName: nil)
                 fetchedResultsController?.delegate = self
             }
         }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        try! fetchedResultsController?.performFetch()
+
+        do {
+            try fetchedResultsController?.performFetch()
+        } catch {
+            print("Error while fetching sightings")
+        }
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+
     }
-    
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         if let frc = fetchedResultsController {
             return frc.sections!.count
         }
         return 0
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         guard let sections = self.fetchedResultsController?.sections else {
             fatalError("No sections in fetchedResultsController")
@@ -47,36 +53,36 @@ class SightingsViewController: UITableViewController, LifelistController {
         let sectionInfo = sections[section]
         return sectionInfo.numberOfObjects
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SightingCell")!
         configure(cell: cell, at: indexPath)
         return cell
     }
-    
+
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         guard let sectionInfo = fetchedResultsController?.sections?[section] else {
             return nil
         }
         return sectionInfo.name
     }
-    
+
     override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
         return fetchedResultsController?.sectionIndexTitles
     }
-    
+
     override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
         guard let result = fetchedResultsController?.section(forSectionIndexTitle: title, at: index) else {
             fatalError("Unable to locate section for \(title) at index: \(index)")
         }
         return result
     }
-    
+
     func configure(cell: UITableViewCell, at indexPath: IndexPath) {
         guard let object = self.fetchedResultsController?.object(at: indexPath) else {
             fatalError("Attempt to configure cell without a managed object")
         }
-        
+
         cell.textLabel?.text = object.species
     }
 
@@ -86,8 +92,10 @@ extension SightingsViewController: NSFetchedResultsControllerDelegate {
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.beginUpdates()
     }
-    
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
         switch type {
         case .insert:
             tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
@@ -97,8 +105,9 @@ extension SightingsViewController: NSFetchedResultsControllerDelegate {
             break
         }
     }
-    
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any,
+                    at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
         switch type {
         case .insert:
             tableView.insertRows(at: [newIndexPath!], with: .fade)
@@ -110,7 +119,7 @@ extension SightingsViewController: NSFetchedResultsControllerDelegate {
             tableView.deleteRows(at: [indexPath!], with: .fade)
         }
     }
-    
+
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.endUpdates()
     }
