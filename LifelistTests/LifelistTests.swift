becd9f32af29cf557dc80cffe66fb0269d2d65cb
Jonathan Arbogast <jonathan.arbogast@gmail.com>
2020-05-08 15:25:17 -0400

    Added a test bundle

---
+++ LifelistTests/LifelistTests.swift
@@ -0,0 +1,17 @@
+//
+//  LifelistTests.swift
+//  LifelistTests
+//
+//  Created by Jonathan Arbogast on 5/8/20.
+//  Copyright © 2020 Jonathan Arbogast. All rights reserved.
+//
+
+import XCTest
+
+class LifelistTests: XCTestCase {
+
+    func testExample() throws {
+        print("Hello Tests!")
+    }
+
+}
